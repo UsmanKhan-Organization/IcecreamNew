9b4bfb21a37224bce32eed26a0676ee4d660045a
Usman Khan <developer.usmankhan@gmail.com>
2021-11-23 00:40:12 +0500

    1

--- Assets/Scripts/GamePlayManager.cs
+++ Assets/Scripts/GamePlayManager.cs
@@ -8,12 +8,11 @@ public class GamePlayManager : MonoBehaviour {
     public LevelSetting[] MyLevelSetting;
     public GameObject Alliey;
     //public GameObject Arrow;
-    public GameObject Player, cutScene8, cutScene9,cutScene10,cutScene11,cutScene12,cutScene13,cutScene14, canVas, garageDoor,garageDoor2;
+    public GameObject Player,canVas;
 
     public GameObject TimerObject, icecreamMan,icecreamManGhost;
-    public GameObject[] cutscene, scenes;
 
-    public GameObject objectPos, jewelleryDoor, level2WalletPos, level6WalletPos, level9WalletPos;
+    public GameObject level2WalletPos, level6WalletPos, level9WalletPos;
     public GameObject dialogueCam, mainCam, level4objects, garageButton, vanDoor, garageWall;
 
 
@@ -63,213 +62,24 @@ public class GamePlayManager : MonoBehaviour {
         // {
         //     canVas.SetActive(true);
         // }
-        if(GameManager.Instance.SelectedLevel == 1)
-        {
-            cutscene[0].SetActive(true);
-            StartCoroutine(Scene2());
-
-        }
-        else if(GameManager.Instance.SelectedLevel == 2)
+        
+        if(GameManager.Instance.SelectedLevel == 2)
         {
             MyLevelSetting[1].Objects[0].transform.position = level2WalletPos.transform.position;
-             cutscene[1].SetActive(true);
-             StartCoroutine(CutsceneDelay());
         }
-        else if(GameManager.Instance.SelectedLevel == 3)
-        {
-            cutscene[2].SetActive(true);
-            StartCoroutine(CutsceneDelay());
-        }
-        else if(GameManager.Instance.SelectedLevel == 4)
-        {
-            cutscene[3].SetActive(true);
-            StartCoroutine(CutsceneDelay());
-        }
-        else if(GameManager.Instance.SelectedLevel == 5)
-        {
-            cutscene[4].SetActive(true);
-            StartCoroutine(CutsceneDelay());
-        }
+        
+        
         else if(GameManager.Instance.SelectedLevel == 6)
         {
             MyLevelSetting[5].Objects[0].transform.position = level6WalletPos.transform.position;
-            cutscene[5].SetActive(true);
-            StartCoroutine(CutsceneDelay());
         }
-        else if(GameManager.Instance.SelectedLevel == 7)
-        {
-            cutscene[6].SetActive(true);
-            StartCoroutine(CutsceneDelay());
-        }
-        else if(GameManager.Instance.SelectedLevel == 8)
-        {
-            cutscene[7].SetActive(true);
-            StartCoroutine(CutsceneDelay());
-        }
+        
+        
         else if(GameManager.Instance.SelectedLevel == 9)
         {
             MyLevelSetting[8].Objects[0].transform.position = level9WalletPos.transform.position;
-            // garageDoor.SetActive(false);
-            // garageDoor2.SetActive(false);
-            cutscene[8].SetActive(true);
-            StartCoroutine(CutsceneDelay());
-        }
-        else if(GameManager.Instance.SelectedLevel == 10)
-        {
-            garageDoor.SetActive(false);
-            cutScene10.SetActive(true);
-            StartCoroutine(CutsceneDelay());
-        }
-        else if(GameManager.Instance.SelectedLevel == 11)
-        {
-            cutScene11.SetActive(true);
-            StartCoroutine(CutsceneDelay());
-        }
-        else if(GameManager.Instance.SelectedLevel == 12)
-        {
-            cutScene12.SetActive(true);
-            StartCoroutine(CutsceneDelay());
-        }
-        else if(GameManager.Instance.SelectedLevel == 13)
-        {
-            cutScene13.SetActive(true);
-            StartCoroutine(CutsceneDelay());
-        }
-        if(GameManager.Instance.SelectedLevel == 14)
-        {
-            MyLevelSetting[13].Objects[0].transform.position = objectPos.transform.position;
-            canVas.SetActive(true);
-        }
-        if(GameManager.Instance.SelectedLevel == 15)
-        {
-            jewelleryDoor.SetActive(false);
-            cutScene14.SetActive(true);
-            StartCoroutine(CutsceneDelay());
         }
 
-    }
-    IEnumerator Cutscene1Delay()
-    {
-        yield return new WaitForSeconds(7);
-        if(scenes[0].activeInHierarchy)
-        {
-            scenes[0].SetActive(false);
-            scenes[1].SetActive(true);
-            canVas.SetActive(true);
-        }
-    }
-    IEnumerator Scene2()
-    {
-        yield return new WaitForSeconds(5);
-        if(scenes[0].activeInHierarchy)
-        {
-            scenes[0].SetActive(false);
-            scenes[1].SetActive(true);
-            StartCoroutine(Scene3());
-        }
-    }
-    IEnumerator Scene3()
-    {
-        yield return new WaitForSeconds(5);
-        if(scenes[1].activeInHierarchy)
-        {
-            scenes[1].SetActive(false);
-            scenes[2].SetActive(true);
-            StartCoroutine(Scene4());
-        }
-    }
-    IEnumerator Scene4()
-    {
-        yield return new WaitForSeconds(5);
-        if(scenes[2].activeInHierarchy)
-        {
-            scenes[2].SetActive(false);
-            scenes[3].SetActive(true);
-            StartCoroutine(Scene5());
-
-        }
-    }
-    IEnumerator Scene5()
-    {
-        yield return new WaitForSeconds(5);
-        if(scenes[3].activeInHierarchy)
-        {
-            cutscene[0].SetActive(false);
-            canVas.SetActive(true);
-        }
-    }
-    IEnumerator CutsceneDelay()
-    {
-        yield return new WaitForSeconds(10);
-        if(cutscene[0].activeInHierarchy)
-        {
-            cutscene[0].SetActive(false);
-        }
-        else if(cutscene[1].activeInHierarchy)
-        {
-            cutscene[1].SetActive(false);
-        }
-        else if(cutscene[2].activeInHierarchy)
-        {
-            cutscene[2].SetActive(false);
-        }
-        else if(cutscene[3].activeInHierarchy)
-        {
-            cutscene[3].SetActive(false);
-        }
-        else if(cutscene[4].activeInHierarchy)
-        {
-            cutscene[4].SetActive(false);
-        }
-        else if(cutscene[5].activeInHierarchy)
-        {
-            cutscene[5].SetActive(false);
-        }
-        else if(cutscene[6].activeInHierarchy)
-        {
-            cutscene[6].SetActive(false);
-        }
-        else if(cutscene[7].activeInHierarchy)
-        {
-            cutscene[7].SetActive(false);
-        }
-        else if(cutscene[8].activeInHierarchy)
-        {
-            garageDoor.SetActive(true);
-            cutscene[8].SetActive(false);
-        }
-        else if(cutScene10.activeInHierarchy)
-        {
-            cutScene10.SetActive(false);
-            garageDoor2.SetActive(false);
-            garageDoor.SetActive(true);
-        }
-        else if(cutScene11.activeInHierarchy)
-        {
-            cutScene11.SetActive(false);
-        }
-        else if(cutScene12.activeInHierarchy)
-        {
-            cutScene12.SetActive(false);
-        }
-        else if(cutScene13.activeInHierarchy)
-        {
-            cutScene13.SetActive(false);
-        }
-        else if(cutScene14.activeInHierarchy)
-        {
-            cutScene14.SetActive(false);
-        }
-        canVas.SetActive(true);
-    }
-    public void OffCutscene()
-    {
-        cutScene8.SetActive(false);
-    }
-    IEnumerator ManEnableDelay()
-    {
-        yield return new WaitForSeconds(10);
-        icecreamMan.SetActive(true);
     }
 
     public void StartLevelSettings()
--- Assets/Timelines.cs
+++ Assets/Timelines.cs
@@ -21,6 +21,108 @@ public class Timelines : MonoBehaviour
             StartCoroutine(Scene1Delay(2.2f));
 
         }
+        else if (GameManager.Instance.SelectedLevel == 2)
+        {
+            cutScene[1].SetActive(true);
+            StartCoroutine(CutSceneDelayandOFF(7));
+
+        }
+        else if (GameManager.Instance.SelectedLevel == 3)
+        {
+            cutScene[2].SetActive(true);
+            StartCoroutine(CutSceneDelayandOFF(5));
+        }
+        else if (GameManager.Instance.SelectedLevel == 4)
+        {
+            cutScene[1].SetActive(true);
+            StartCoroutine(CutSceneDelayandOFF(5));
+
+        }
+        else if (GameManager.Instance.SelectedLevel == 5)
+        {
+            cutScene[2].SetActive(true);
+            StartCoroutine(CutSceneDelayandOFF(11));
+        }
+        else if (GameManager.Instance.SelectedLevel == 6)
+        {
+            cutScene[1].SetActive(true);
+            StartCoroutine(CutSceneDelayandOFF(11));
+
+        }
+        else if (GameManager.Instance.SelectedLevel == 7)
+        {
+            cutScene[2].SetActive(true);
+            StartCoroutine(CutSceneDelayandOFF(7));
+        }
+        else if (GameManager.Instance.SelectedLevel == 8)
+        {
+            cutScene[1].SetActive(true);
+            StartCoroutine(CutSceneDelayandOFF(7));
+
+        }
+        else if (GameManager.Instance.SelectedLevel == 9)
+        {
+            cutScene[2].SetActive(true);
+            StartCoroutine(CutSceneDelayandOFF(7));
+        }
+        else if (GameManager.Instance.SelectedLevel == 10)
+        {
+            cutScene[2].SetActive(true);
+            StartCoroutine(CutSceneDelayandOFF(11));
+        }
+    }
+    IEnumerator CutSceneDelayandOFF(int Delay)
+    {
+        yield return new WaitForSeconds(Delay);
+        if(cutScene[1].activeInHierarchy)
+        {
+            cutScene[1].SetActive(false);
+        }
+        else if(cutScene[2].activeInHierarchy)
+        {
+            cutScene[2].SetActive(false);
+        }
+        else if(cutScene[3].activeInHierarchy)
+        {
+            cutScene[3].SetActive(false);
+        }
+        else if(cutScene[4].activeInHierarchy)
+        {
+            cutScene[4].SetActive(false);
+        }
+        else if(cutScene[5].activeInHierarchy)
+        {
+            cutScene[5].SetActive(false);
+        }
+        else if(cutScene[6].activeInHierarchy)
+        {
+            cutScene[6].SetActive(false);
+        }
+        else if(cutScene[7].activeInHierarchy)
+        {
+            cutScene[7].SetActive(false);
+        }
+        else if(cutScene[8].activeInHierarchy)
+        {
+            cutScene[8].SetActive(false);
+        }
+        else if(cutScene[9].activeInHierarchy)
+        {
+            cutScene[9].SetActive(false);
+        }
+        else if(cutScene[10].activeInHierarchy)
+        {
+            cutScene[10].SetActive(false);
+        }
+        GamePlayManager.instance.canVas.SetActive(true);
+        
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+
     }
     IEnumerator Scene1Delay(float time)
     {
@@ -60,12 +162,5 @@ public class Timelines : MonoBehaviour
         yield return new WaitForSeconds(bTime);
         sTone.SetActive(true);
         StartCoroutine(Scene2Delay(2.8f));
-    }
-
-
-    // Update is called once per frame
-    void Update()
-    {
-
     }
 }
